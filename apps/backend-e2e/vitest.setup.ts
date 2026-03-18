373d8afcf8c2c70d0de218d4100f61c17c032c60
Kasir Barati <kasir.barati@gmail.com>
2026-03-19 00:01:13 +0100

    chore: rename PORT to BACKEND_PORT

--- apps/backend-e2e/vitest.setup.ts
+++ apps/backend-e2e/vitest.setup.ts
@@ -7,5 +7,5 @@ expect.extend(matchers);
 
 // Configure axios for tests to use.
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ?? '3000';
+const port = process.env.PORT ?? '8080';
 axios.defaults.baseURL = `http://${host}:${port}`;
