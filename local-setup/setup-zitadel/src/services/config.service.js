7e670a6a90b72bdf246d9816ab277cf107d29387
Kasir Barati <kasir.barati@gmail.com>
2026-03-28 11:21:20 +0100

    chore: enforce reading app name & zitadel URL from env

--- local-setup/setup-zitadel/src/services/config.service.js
+++ local-setup/setup-zitadel/src/services/config.service.js
@@ -3,9 +3,20 @@
 import { mkdirSync } from 'fs';
 import { join } from 'path';
 
+import { isEmpty } from '../utils/index.js';
+
 export const zitadelDir = '/zitadel-pat';
 const clientDir = join(zitadelDir, 'client');
 const integrationTestDir = join(zitadelDir, 'integration-test');
+const appName = process.env.ZITADEL_APP_NAME;
+const zitadelUrl = process.env.ZITADEL_URL;
+
+if (isEmpty(appName)) {
+  throw new Error('ZITADEL_APP_NAME environment variable is not set');
+}
+if (isEmpty(zitadelUrl)) {
+  throw new Error('ZITADEL_URL environment variable is not set');
+}
 
 export class ConfigService {
   constructor() {
@@ -13,20 +24,23 @@ export class ConfigService {
     mkdirSync(integrationTestDir, { recursive: true });
   }
 
-  appName = process.env.APP_NAME ?? 'smart-novel-app';
+  appName = appName;
+  zitadelUrl = zitadelUrl;
   projectName = `${this.appName}-project`;
-  zitadelUrl = process.env.ZITADEL_URL ?? 'http://localhost:8080';
 
   patFile = join(zitadelDir, 'token');
   projectIdFile = join(zitadelDir, 'project-id');
 
-  clientIdFile = join(clientDir, `${this.appName}-id`);
+  clientIdFile = join(clientDir, `${this.appName}-client-id`);
 
   integrationTest = {
     appName: `${this.appName}-integration-test`,
     botPatFile: join(integrationTestDir, 'bot-token'),
     botKeyPath: join(integrationTestDir, 'bot-key.json'),
-    clientIdFile: join(integrationTestDir, `${this.appName}-id`),
+    clientIdFile: join(
+      integrationTestDir,
+      `${this.appName}-client-id`,
+    ),
     clientSecretFile: join(
       integrationTestDir,
       `${this.appName}-secret`,
